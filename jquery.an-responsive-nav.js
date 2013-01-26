8f97a7ec43fbbdfdfac289fcb040ae700164571d
angled <prakash17023005>
2013-01-26 09:45:49 +0100

    commit in submodule

--- jquery.an-responsive-nav.js
+++ jquery.an-responsive-nav.js
@@ -14,6 +14,7 @@
                 }
             }, options);
 
+		var test;
         var $this = jQuery(this);
         var $thisUl = jQuery(this).children('ul');
         var $thisFirstLevelLi = $thisUl.children('li').addClass('first-level');
